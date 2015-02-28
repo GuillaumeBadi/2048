616fc58f94768dd2f9d1fd120d8374e30c53ffb9
Bruno <sautron.brunojj@gmail.com>
2015-02-28 05:09:14 +0100

    arfarf:

--- display/main.c
+++ display/main.c
@@ -1,22 +1,10 @@
 #include "rush.h"
 
-int		main(void)
+void	ft_draw_col(WINDOW *field, int y_max, int x_max)
 {
-	int x_max;
-	int	y_max;
-	int	x;
-	int	y;
-	WINDOW *field;
+	int		y;
+	int		x;
 
-	initscr();
-	noecho();
-	curs_set(FALSE);
-	getmaxyx(stdscr, y_max, x_max);
-	field = newwin(y_max, x_max, 0, 0);
-	x_max /= 4;
-	y_max /= 4;
-	x_max *= 4;
-	y_max *= 4;
 	y = 0;
 	while (y <= y_max)
 	{
@@ -28,6 +16,13 @@ int		main(void)
 		}
 		y++;
 	}
+}
+
+void	ft_draw_row(WINDOW *field, int y_max, int x_max)
+{
+	int		y;
+	int		x;
+
 	y = 0;
 	while (y <= y_max)
 	{
@@ -39,6 +34,13 @@ int		main(void)
 		}
 		y += y_max / 4;
 	}
+}
+
+void	ft_draw_corner(WINDOW *field, int y_max, int x_max)
+{
+	int		y;
+	int		x;
+
 	y = 0;
 	while (y <= y_max)
 	{
@@ -50,9 +52,36 @@ int		main(void)
 		}
 		y += y_max / 4;
 	}
+}
+
+void	ft_draw_grid(WINDOW *field, int y_max, int x_max)
+{
+	ft_draw_col(field, y_max, x_max);
+	ft_draw_row(field, y_max, x_max);
+	ft_draw_corner(field, y_max, x_max);
+}
+
+int		main(void)
+{
+	WINDOW *field;
+	int		y_max;
+	int		x_max;
 
 
+	initscr();
+	noecho();
+	curs_set(FALSE);
+	getmaxyx(stdscr, y_max, x_max);
+	x_max /= 4;
+	y_max /= 4;
+	x_max *= 4;
+	y_max *= 4;
+	y_max++;
+	x_max++;
+	field = newwin(y_max, x_max, 0, 0);
+	ft_draw_grid(field, y_max, x_max);
 	wrefresh(field);
+
 	sleep(5);
 	delwin(field);
 	endwin();
