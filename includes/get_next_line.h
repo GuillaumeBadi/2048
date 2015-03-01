ab3c2347ce4a46ed2bd29c29e616dd2b77fcb00d
gbadi <gbadi@student.42.fr>
2015-03-01 21:59:51 +0100

    okok

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbadi <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: gbadi <gbadi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/10 01:32:34 by gbadi             #+#    #+#             */
-/*   Updated: 2015/01/27 22:10:05 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/03/01 21:34:04 by gbadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include "libft.h"
 
 # define BUFF_SIZE			4096
-# define FT_SWAP(x, y, z) {z = x; x = y; if (z != x) {free(z);}}
+# define FT_SWAP(x, y, z) 	{z = x; x = y; if (z != x) {free(z);}}
 
 typedef struct				s_buff
 {
@@ -25,6 +26,6 @@ typedef struct				s_buff
 	int						len;
 }							t_buff;
 
-int						get_next_line(int const fd, char **line);
+int							get_next_line(int const fd, char **line);
 
 #endif
--- print3.c
+++ print3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvolberg <dvolberg@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gbadi <gbadi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/01 05:01:36 by dvolberg          #+#    #+#             */
-/*   Updated: 2015/03/01 19:40:46 by dvolberg         ###   ########.fr       */
+/*   Updated: 2015/03/01 21:27:51 by gbadi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void ascii_2048(int y, int x)
 {
 	attron(COLOR_PAIR(COLOR_2048));
-	ascii_2(y, x + (SIZE- 34 / 2));
+	ascii_2(y, x + (SIZE - 34 / 2));
 	ascii_0(y, x + (SIZE - 34 / 2) + 6);
-	ascii_4(y, x + SIZE - 34 / 2+ 14);
-	ascii_8(y, x + SIZE - 34 / 2+ 21);
+	ascii_4(y, x + SIZE - 34 / 2 + 14);
+	ascii_8(y, x + SIZE - 34 / 2 + 21);
 	attroff(COLOR_PAIR(COLOR_2048));
 }
